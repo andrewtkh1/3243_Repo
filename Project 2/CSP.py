90419430c6306b9fae6666e8762d8b08f143ec05
andrewtkh1 <andrewtkh1@gmail.com>
2022-03-10 19:45:19 +0800

    Edited code to Var = Pieces. Domain = Pos

--- Project 2/CSP.py
+++ Project 2/CSP.py
@@ -18,6 +18,7 @@ class InitParams:
     dictOfObsOnBoard = {}
 
 class Board:
+    dictOfPriority = {'King': 2, 'Queen':0, 'Bishop': 1, 'Rook': 1, 'Knight': 2}
     maxRows = 0
     maxCols = 0
     totalThreat = 0
@@ -76,7 +77,7 @@ class Moves:
         pos = arrToChessPos(x,y)
         if (dictOfCurBoard.get(pos,0) == -1):
             return True
-        if (y > maxRow or y < 0 or numOfMoves == 0 or dictOfCurBoard.get(pos,0) == -3):
+        if (y > maxRow or y < 0 or numOfMoves == 0 or dictOfCurBoard.get(pos,0) == -3): #-3 is obstacles
             return False
         dictOfCurBoard[pos] = -2
         numOfMoves-=1
@@ -177,7 +178,7 @@ def run_CSP():
     Board.maxCols = InitParams.cols - 1
     Board.maxRows = InitParams.rows - 1
     dictOfCurBoard = InitParams.dictOfObsOnBoard
-    newSearch(dictOfCurBoard, Board.dictOfPieces, Board.countDict)
+    search(dictOfCurBoard, Board.dictOfPieces, Board.countDict)
     return Board.dictOfPieces
 
 def getNumOfPiece(name, dict):
@@ -188,195 +189,127 @@ def getNumOfPiece(name, dict):
             counter+=1
     return counter
     
-def newSearch(dictOfCurBoard, dictOfPieces, dictOfNumberOfPiece):
-        masterPQ = []
-        heapq.heapify(masterPQ)
-        if (len(dictOfPieces) == InitParams.totalOwnPiece): #Goal check.
-            Board.dictOfPieces = dictOfPieces
+def search(dictOfCurBoard, dictOfPieces, dictOfNumberOfPiece):
+    
+    if (len(dictOfPieces) == InitParams.totalOwnPiece): #Goal check.
+        Board.dictOfPieces = dictOfPieces
+        return True    
+    
+    lsOfSpots = []
+    heapq.heapify(lsOfSpots)
+    pieceList = getLeastRemainingValuePos(dictOfCurBoard, dictOfNumberOfPiece)
+    if (pieceList == -1): #No valid assignment of piece
+        return False
+    (noOfPosValid, priority, data) = pieceList
+    (nameOfPiece, lsOfSpots) = data
+    
+    trialDictOfPieces = copy.copy(dictOfPieces)
+    trialDictOfNumberOfPieces = copy.copy(dictOfNumberOfPiece)
+    
+    #lsOfSpots = getLeastConstrainValueVar(nameOfPiece, dictOfCurBoard)
+    
+    while (len(lsOfSpots) > 0):
+        vertex = heapq.heappop(lsOfSpots)
+        (numOfPosBlocked, node) = vertex
+        (pos, preDoneBoard) = node
+        curTuple = (pos[0], int(pos[1:]))
+        trialDictOfPieces[curTuple] = nameOfPiece #Dict to print
+        trialDictOfNumberOfPieces[pos] = nameOfPiece #Dict to keep track of what pieces are on the board.
+        #print(trialDictOfPieces)
+        result = search(preDoneBoard, trialDictOfPieces, trialDictOfNumberOfPieces) #change trialB to trial board
+        if (result):
             return True
+        trialDictOfPieces = copy.copy(dictOfPieces)
+        trialDictOfNumberOfPieces = copy.copy(dictOfNumberOfPiece)
         
-        masterPQ = getLeastRemainingValuePos(dictOfCurBoard, dictOfNumberOfPiece)
-        if (masterPQ == -1):
-            return False #No more piece to put.
-        
-        curNumOfPc = 0
-        
-        for nameOfPiece in Board.listOfRemainingPieces: #Calc the current number of pieces on board
-            curNumOfPc = curNumOfPc + getNumOfPiece(nameOfPiece,dictOfNumberOfPiece)
-        
-        while (len(masterPQ) > 0 and len(masterPQ) >= InitParams.totalOwnPiece - curNumOfPc):
-            
-            datas = heapq.heappop(masterPQ)
-            (count, deg, packet) = datas
-            (curPos, pqOfPiece) = packet
-            #(curPos, pqOfPieces) = datas
+    return False
 
-            #pqOfPiece = getLeastConstrainValueVar(curPos, dictOfCurBoard, dictOfNumberOfPiece)
-            trialBoard = copy.copy(dictOfCurBoard)
-            trialDictOfPieces = copy.copy(dictOfPieces)
-            trialDictOfNumberOfPieces = copy.copy(dictOfNumberOfPiece)
-            while (len(pqOfPiece) > 0):
-                #(val,curPc) = heapq.heappop(pqOfPiece)
-                #hasFailed = checkAndPlacePiece(curPc, curPos, trialBoard)
-                hasFailed = False
-                (val,data) = heapq.heappop(pqOfPiece)
-                (curPc, trialB) = data
-                if (not hasFailed):
-                    # Successful place
-                    curTuple = (curPos[0], int(curPos[1:]))
-                    trialDictOfPieces[curTuple] = curPc
-                    trialDictOfNumberOfPieces[curPos] = curPc
-                    print(trialDictOfPieces)
-                    result = newSearch(trialB, trialDictOfPieces, trialDictOfNumberOfPieces) #change trialB to trial board
-                    if (result):
-                        return True
-                    else:
-                        trialBoard = copy.copy(dictOfCurBoard)
-                        trialDictOfPieces = copy.copy(dictOfPieces)
-                        trialDictOfNumberOfPieces = copy.copy(dictOfNumberOfPiece)
-                # All options failed
-                trialBoard[curPos] = 5 # 5 to means must be empty
-        return #newSearch(trialBoard, dictOfPieces, dictOfNumberOfPiece)
-
-def getLeastConstrainValueVar(pos, dictOfCurBoard, dictOfNumberOfPieces):
+def getLeastConstrainValueVar(piece, dictOfCurBoard):
     curSpotLeft = 0
     listOfPc = []
-    dictOfSumOfPiece = {}
     heapq.heapify(listOfPc)
     trialBoard = copy.copy(dictOfCurBoard)
-    
-    for nameOfPiece in Board.listOfRemainingPieces: #Calc the current number of pieces on board
-        val = getNumOfPiece(nameOfPiece,dictOfNumberOfPieces)
-        dictOfSumOfPiece[nameOfPiece] = val
-        
-    for nameOfPc in Board.listOfRemainingPieces:
-        if (dictOfSumOfPiece.get(nameOfPc) >= Board.dictOfMaxPiece.get(nameOfPc)): #Check if num of piece has reached the max count
-                continue
-        if (not checkAndPlacePiece(nameOfPc, pos, trialBoard)):
-            curSpotLeft = len(trialBoard)
-            #node = (curSpotLeft, nameOfPc)
-            data = (nameOfPc,trialBoard)
-            node = (curSpotLeft,data)
-            heapq.heappush(listOfPc,node)
-        trialBoard = copy.copy(dictOfCurBoard)
-    return listOfPc
-        
-def getLeastRemainingValuePos(dictOfCurBoard, dictOfNumberOfPieces):
-    count = 0
-    minCount = 99999
     x = 0
     y = 0
-    listOfPos = []
-    heapq.heapify(listOfPos)
-    dictOfSumOfPiece = {}
-    for nameOfPiece in Board.listOfRemainingPieces: #Calc the current number of pieces on board
-        val = getNumOfPiece(nameOfPiece,dictOfNumberOfPieces)
-        dictOfSumOfPiece[nameOfPiece] = val
-
+    listOfPcs = []
+    heapq.heapify(listOfPcs)
     while (y <= Board.maxCols):
         pos = arrToChessPos(x, y)
-        if (pos in dictOfCurBoard): #if position is blocked/threat (5 means allocated as empty)
+        if (pos in dictOfCurBoard): #if position is blocked/threat
             x+=1
             if (x > Board.maxCols):
                 x = 0
                 y+=1
             continue
-            
-        #Portion for LCV
-        curSpotLeft = 0
-        listOfPc = []
-        heapq.heapify(listOfPc)
-
-        for nameOfPiece in Board.listOfRemainingPieces:            
-            if (dictOfSumOfPiece.get(nameOfPiece) >= Board.dictOfMaxPiece.get(nameOfPiece)): #Check if num of piece has reached the max count
-                continue
-            trialBoard = copy.copy(dictOfCurBoard)
-            isNotValidPlace = checkAndPlacePiece(nameOfPiece, pos, trialBoard)
-            if (not isNotValidPlace):
-                count+=1
-                #Portion for LCV
-                curSpotLeft = len(trialBoard)
-                data = (nameOfPiece,trialBoard)
-                node = (curSpotLeft,data)
-                heapq.heappush(listOfPc,node)
-        if (count > 0):
-            heapq.heappush(listOfPos,(count,(pos,listOfPc))) #count must be min
-        # if (count == minCount and count > 0):
-        #     listOfPos.append((pos,listOfPc))
-        # elif (count < minCount and count > 0):
-        #     listOfPos = []
-        #     minCount = count
-        #     listOfPos.append((pos,listOfPc))
+        trialBoard = copy.copy(dictOfCurBoard)
+        isNotValidPlace = checkAndPlacePiece(piece, pos, trialBoard)
+        if (not isNotValidPlace):
+            constr = len(trialBoard)
+            node = (pos, trialBoard)
+            heapq.heappush(listOfPc, (constr, node))
         x+=1
         if (x > Board.maxCols):
             x = 0
             y+=1
-        count = 0
+        trialBoard = copy.copy(dictOfCurBoard)
+    return listOfPc
+        
 
-    degree = 0
-    maxDegree = 0
-    finPos = []
-    heapq.heapify(finPos)
+# Changed to piece. For each piece, calc how many spots on the board is valid for it. Get piece with the min.
+# Return (numOfPositionsValid, Board.dictOfPriority.get(uniquePiece), (uniquePiece, listOfSpots))
+# lsOfSpots is =  node = (numOfPosBlocked,data), data = (pos,trialBoard)
+def getLeastRemainingValuePos(dictOfCurBoard, dictOfNumberOfPieces):
+    numOfPositionsValid = 0
+    minNumOfPositionsValid = 99999
+
+    listOfPcs = []
+    heapq.heapify(listOfPcs)
+    dictOfSumOfPiece = {}
+    dictOfDistinctPiece = {}
     
-    # Max Degree Heuristic
-    for curData in listOfPos:
-        (count, cur) = curData
-        (curPos, ls) = cur 
-        (x,y) = chessPosToArr(curPos)
-        if (x + 1 <= Board.maxCols):
-            incPos = arrToChessPos(x+1, y)
-            val = dictOfCurBoard.get(incPos,0)
-            if (not val == -3):
-                degree+=1
-        if (x + 1 <= Board.maxCols and y + 1 <= Board.maxRows):
-            incPos = arrToChessPos(x+1, y+1)
-            val = dictOfCurBoard.get(incPos,0)
-            if (not val == -3):
-                degree+=1
-        if (y + 1 <= Board.maxRows):
-            incPos = arrToChessPos(x, y+1)
-            val = dictOfCurBoard.get(incPos,0)
-            if (not val == -3):
-                degree+=1
-        if (x - 1 >= 0 and y + 1 <= Board.maxRows):
-            incPos = arrToChessPos(x-1, y+1)
-            val = dictOfCurBoard.get(incPos,0)
-            if (not val == -3):
-                degree+=1
-        if (x - 1 >= 0):
-            incPos = arrToChessPos(x-1, y)
-            val = dictOfCurBoard.get(incPos,0)
-            if (not val == -3):
-                degree+=1
-        if (x - 1 >= 0 and y - 1 >= 0):
-            incPos = arrToChessPos(x-1, y-1)
-            val = dictOfCurBoard.get(incPos,0)
-            if (not val == -3):
-                degree+=1
-        if (y - 1 >= 0):
-            incPos = arrToChessPos(x, y-1)
-            val = dictOfCurBoard.get(incPos,0)
-            if (not val == -3):
-                degree+=1
-        if (x + 1 <= Board.maxCols and y - 1 >= 0):
-            incPos = arrToChessPos(x+1, y-1)
-            val = dictOfCurBoard.get(incPos,0)
-            if (not val == -3):
-                degree+=1
-        if (degree > 0):
-            heapq.heappush(finPos, (count, -degree, (curPos,ls)))
-        # if (degree > maxDegree and degree > 0):
-        #     maxDegree = degree
-        #     finPos = []
-        #     finPos.append((curPos,ls))
-        # elif (degree == maxDegree and degree != 0):
-        #     finPos.append((curPos,ls))
-        degree = 0
-    
-    if (len(finPos) == 0):
+    for nameOfPiece in Board.listOfRemainingPieces: #Calc the current number of pieces on board
+        val = getNumOfPiece(nameOfPiece,dictOfNumberOfPieces)
+        if (dictOfDistinctPiece.get(nameOfPiece,"0") == "0"): # Add each unique piece.
+            dictOfDistinctPiece[nameOfPiece] = -1
+        dictOfSumOfPiece[nameOfPiece] = val
+        
+    for uniquePiece in dictOfDistinctPiece:
+        x = 0
+        y = 0
+        if (dictOfSumOfPiece.get(uniquePiece) >= Board.dictOfMaxPiece.get(uniquePiece)): #Check if num of piece has reached the max count
+            continue
+        listOfSpots = []
+        heapq.heapify(listOfSpots)
+        while (y <= Board.maxCols):
+            pos = arrToChessPos(x, y)
+            if (pos in dictOfCurBoard): #if position is blocked/threat
+                x+=1
+                if (x > Board.maxCols):
+                    x = 0
+                    y+=1
+                continue
+            #Portion for LCV
+            trialBoard = copy.copy(dictOfCurBoard)
+            isNotValidPlace = checkAndPlacePiece(uniquePiece, pos, trialBoard)
+            if (not isNotValidPlace):
+                numOfPositionsValid+=1
+                #Portion for LCV
+                numOfPosBlocked = len(trialBoard)
+                data = (pos,trialBoard)
+                node = (numOfPosBlocked,data) # We want the least number of spots taken
+                heapq.heappush(listOfSpots,node)
+            x+=1
+            if (x > Board.maxCols):
+                x = 0
+                y+=1
+                
+        if (numOfPositionsValid > 0):
+                # Priority is for max degree Heuristic.
+                heapq.heappush(listOfPcs,(numOfPositionsValid,Board.dictOfPriority.get(uniquePiece),(uniquePiece,listOfSpots))) #count must be min        
+        numOfPositionsValid = 0
+    if (len(listOfPcs) == 0):
         return -1
-
-    return finPos
+    return heapq.heappop(listOfPcs)
 
 def checkAndPlacePiece(piece, pos, dictOfCurBoard):
     status = False #True means is not valid place.
